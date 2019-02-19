848e95ec8c7fb17a9377639cb862101d87cb7d7c
Stefany2210 <stefany.liseth@gmail.com>
2019-02-19 10:46:51 -0500

    trivia

---
+++ app.js
@@ -0,0 +1,6 @@
+// Preguntar la temperatura el celsius
+var temperature = parseInt(prompt("¿Cuál es la temperatura en celsius?"));
+// Convertir la tempratura a Farenheit
+var converToFarenheit = (temperature*1.8) + 32;
+// Mostrar un resultado en la consola
+console.log("La temperatura en Farenheit es " + converToFarenheit);
